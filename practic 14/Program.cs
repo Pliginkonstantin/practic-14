dce01cc5635928ae362cb264fcad18371cc7b6fd
Pliginkonstantin <falsekiller3@gmail.com>
2024-12-17 09:05:59 +0200

    classWork 1

---
+++ practic 14/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practic_14
+{
+    /// <summary>
+    /// Задание 1. Вывести на экран в столбик первые 10 натуральных чисел (циклы с условием, цикл с параметром)
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.Read();
+        }
+        //static void Func()
+        //{
+        //    int x = 1;
+        //    while (x <= 10)
+        //    {
+        //        Console.WriteLine(x);
+        //        x++;
+        //    }
+        //}
+
+        //static void Func()
+        //{
+        //    int x = 1;
+        //    do
+        //    {
+        //        Console.WriteLine(x);
+        //        x++;
+        //    }
+        //    while (x <= 10);
+        //}
+
+        static void Func()
+        {
+            
+            for (int x = 10; x >= 1 ; x--)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
